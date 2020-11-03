20c77c9cb3567b3c391ef109e12f5d18b96121f4
Michal Miszczyszyn <michal@mmiszy.pl>
2020-11-03 17:47:57 +0100

    fix

--- add-env.sh
+++ add-env.sh
@@ -5,11 +5,19 @@ echo '#!/bin/bash' > $(dirname $0)/tmp.sh
 echo 'set -e' >> $(dirname $0)/tmp.sh
 echo '' >> $(dirname $0)/tmp.sh
 
-# https://github.com/JimCronqvist/action-ssh/blob/7737f1192ddd8376686e9d6354dea44592c942bf/entrypoint.sh#L14-L21
 echo '# Environment variables:' >> $(dirname $0)/tmp.sh
-declare -px | while read line; do
-  echo $line >> $(dirname $0)/tmp.sh
-done
+
+echo $WWW_SUBDOMAIN > >> $(dirname $0)/tmp.sh
+echo $API_SUBDOMAIN > >> $(dirname $0)/tmp.sh
+echo $BRANCH > >> $(dirname $0)/tmp.sh
+echo $DOMAIN > >> $(dirname $0)/tmp.sh
+echo $PROJECT_DIRECTORY > >> $(dirname $0)/tmp.sh
+echo $ENV > >> $(dirname $0)/tmp.sh
+
+# declare -px | while read line; do
+#   echo $line >> $(dirname $0)/tmp.sh
+# done
+
 echo '' >> $(dirname $0)/tmp.sh
 echo '# Commands:' >> $(dirname $0)/tmp.sh
 cat $(dirname $0)/_ssh-script-deploy.sh >> $(dirname $0)/tmp.sh
