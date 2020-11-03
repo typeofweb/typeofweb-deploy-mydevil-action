7217da2bd4c00bb2c4ef2a9d1a3251b641e2e9d6
Michal Miszczyszyn <michal@mmiszy.pl>
2020-11-03 17:22:02 +0100

    Fix

--- add-env.sh
+++ add-env.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 set -e
 
-echo "BASH_SOURCE" ${BASH_SOURCE}
-echo "0" $0
-echo "pwd" $(pwd)
-
-echo '#!/bin/bash' > ./tmp.sh
-echo 'set -e' >> ./tmp.sh
-echo '' >> ./tmp.sh
+echo '#!/bin/bash' > $(dirname $0)/tmp.sh
+echo 'set -e' >> $(dirname $0)/tmp.sh
+echo '' >> $(dirname $0)/tmp.sh
 
 # https://github.com/JimCronqvist/action-ssh/blob/7737f1192ddd8376686e9d6354dea44592c942bf/entrypoint.sh#L14-L21
-echo '# Environment variables:' >> ./tmp.sh
+echo '# Environment variables:' >> $(dirname $0)/tmp.sh
 declare -px | while read line; do
-  echo $line >> ./tmp.sh
+  echo $line >> $(dirname $0)/tmp.sh
 done
-echo '' >> ./tmp.sh
-echo '# Commands:' >> ./tmp.sh
-cat ./_ssh-script-deploy.sh >> ./tmp.sh
+echo '' >> $(dirname $0)/tmp.sh
+echo '# Commands:' >> $(dirname $0)/tmp.sh
+cat $(dirname $0)/_ssh-script-deploy.sh >> $(dirname $0)/tmp.sh
 
-mv ./tmp.sh ./ssh-script-deploy.sh
+mv $(dirname $0)/tmp.sh $(dirname $0)/ssh-script-deploy.sh
